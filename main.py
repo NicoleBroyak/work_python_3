9dc88dc3914b17837d24d48720aaceca014f8b75
Nikola Broyak <nikolabroyak@gmail.com>
2021-08-04 15:07:06 +0200

    Addition of feature: 1. The heaviest package and its number; 2. Min and max item weight are not defined by default as well as max package weight

--- main.py
+++ main.py
@@ -5,42 +5,49 @@ item_weight = 1
 count = 0
 current_package_weight = 0
 package_number = 1
-lightest_package = 20
+min_item_weight = int(input("Type minimal weight of item: "))
+max_item_weight = int(input("Type maximum weight of item: "))
+package_weight_limit = int(input("Type maximum weight of package: "))
+lightest_package = package_weight_limit
 lightest_package_number = 1
-# ^addition of initial variables
+heaviest_package = 0
+heaviest_package_number = 1
 while item_weight:
     item_weight = int(input("Please type weight of item to add to package"
                             " number {}:".format(package_number)))
 
-    if item_weight > 10 or item_weight < 0:          # Protection in case of
-        print("Please type correct weight of item")  # typing wrong weight
-        continue                                     #
+    if ((item_weight > max_item_weight) and (item_weight != 0) or
+       (item_weight < min_item_weight) and (item_weight != 0)):
+        print("Please type correct weight of item")
+        continue
 
     kg_sent += item_weight
+    if item_weight == 0 and current_package_weight != 0 and count > 0:
+        packages_sent += 1
+        unused_kg += (package_weight_limit - current_package_weight)
+        break
 
-    if item_weight == 0 and current_package_weight != 0 and count > 0:  # this
-        packages_sent += 1                        # block ensures that partially
-        unused_kg += 20 - current_package_weight  # filled package is included
-        break                                     # in final calculation
-
-    if (current_package_weight + item_weight) > 20:    # This block ensures
-        package_number += 1                            # that the package weight
-        packages_sent += 1                             # won't exceed 20 kg
-        unused_kg += 20 - current_package_weight       # and switches to next
-        if lightest_package > current_package_weight:  # package while exceeded
-            lightest_package = current_package_weight  # and notes whether the
-            lightest_package_number = package_number   # package is the lightest
-        current_package_weight = item_weight           # and adds too heavy item
-        count += 1                                     # to next package
+    if (current_package_weight + item_weight) > package_weight_limit:
+        if lightest_package > current_package_weight:
+            lightest_package = current_package_weight
+            lightest_package_number = package_number
+        if heaviest_package < current_package_weight:
+            heaviest_package = current_package_weight
+            heaviest_package_number = package_number
+        package_number += 1
+        packages_sent += 1
+        unused_kg += package_weight_limit - current_package_weight
+        current_package_weight = item_weight
+        count += 1
         print("item too heavy for current package, switching to next package, "
               "current package weight is {}".format(current_package_weight))
-        continue                                       #
-    else:                                              #
+        continue
+    else:
         current_package_weight = current_package_weight + item_weight
-        if item_weight != 0:                           #
-            count += 1                                 #
+        if item_weight != 0:
+            count += 1
         print("Item added to package successfully, current package weight is "   
-              " {} kg".format(current_package_weight))  # end of block
+              " {} kg".format(current_package_weight))
 
 if item_weight == 0 and count == 0:
     print("Packages sent: 0; kilograms sent: 0; unused kg in packages sent: 0; "
@@ -48,14 +55,22 @@ if item_weight == 0 and count == 0:
 
 if count != 0:
     print("Process finished, sending packages and calculating results")
+    print(lightest_package_number)
+    print(heaviest_package_number)
     if lightest_package > current_package_weight:
         lightest_package = current_package_weight
         lightest_package_number = package_number
+    if heaviest_package < current_package_weight:
+        heaviest_package = current_package_weight
+        heaviest_package_number = package_number
+    print(lightest_package_number)
+    print(heaviest_package_number)
     print("Packages sent: {}; kilograms sent: {};"
           " unused kg in packages sent: {}; the lightest package sent:"
-          "number {} ({}kg))"
+          "number {} ({}kg); the heaviest package sent: number {} ({}kg)"
           .format(packages_sent, kg_sent, unused_kg,
-                  lightest_package_number, lightest_package))
+                  lightest_package_number, lightest_package,
+                  heaviest_package_number, heaviest_package))
 '''
 Made
 by 
